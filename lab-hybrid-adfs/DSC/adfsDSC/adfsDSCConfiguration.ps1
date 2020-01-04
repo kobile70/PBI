0cd62c9fdb7f64bae6ea546adb1e7086d7ba8800
kobile70 <59473017+kobile70@users.noreply.github.com>
2020-01-04 14:31:02 +0200

    Add files via upload

--- lab-hybrid-adfs/DSC/adfsDSC/adfsDSCConfiguration.ps1
+++ lab-hybrid-adfs/DSC/adfsDSC/adfsDSCConfiguration.ps1
@@ -61,30 +61,5 @@ Configuration Main
                 return ($cert -ine $null)   #if not null (if we have the cert) return true
             }
         }
-
-        Script InstallAADConnect
-        {
-            SetScript = {
-                $AADConnectDLUrl="https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
-                $exe="$env:SystemRoot\system32\msiexec.exe"
-
-                $tempfile = [System.IO.Path]::GetTempFileName()
-                $folder = [System.IO.Path]::GetDirectoryName($tempfile)
-
-                $webclient = New-Object System.Net.WebClient
-                $webclient.DownloadFile($AADConnectDLUrl, $tempfile)
-
-                Rename-Item -Path $tempfile -NewName "AzureADConnect.msi"
-                $MSIPath = $folder + "\AzureADConnect.msi"
-
-                Invoke-Expression "& `"$exe`" /i $MSIPath /qn /passive /forcerestart"
-            }
-
-            GetScript =  { @{} }
-            TestScript = { 
-                return Test-Path "$env:TEMP\AzureADConnect.msi" 
-            }
-            DependsOn  = '[Script]SaveCert','[WindowsFeature]installADFS'
-        }
     }
 }
